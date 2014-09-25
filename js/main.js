08c8b50befaf2a47d8469dc6200cd99df86f0ffe
oxoxll <oxoxll@qq.com>
2014-09-25 14:22:36 +0800

    2048

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ var game = new Game($('#game'));
 $('#newgame').click(function(){
     game.init();
 });
+
+$.fn.extend({
+	move: function(oldI, oldJ, newI, newJ, direction){
+		return this.each(function() {
+			var width = parseInt($(this).css('width'));
+			if('left' == direction || 'right' == direction){
+				var left = parseInt($(this).css('left'));
+				$(this).css('left', (left + (newJ-oldJ) * width * 24 / 20) + 'px');
+			}else{
+				var top = parseInt($(this).css('top'));
+				$(this).css('top', (top + (newI-oldI) * width * 24 / 20) + 'px');
+			}
+	    });
+	}
+});
+$('#test').css('left', '100px');
